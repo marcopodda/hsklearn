e8d9971c86f855806345ffde583be53daf43d334
Marco Podda <marco.x.podda@gsk.com>
2023-03-14 18:52:52 +0100

    added split basic config

--- src/hsklearn/evaluate.py
+++ src/hsklearn/evaluate.py
@@ -1,2 +1,22 @@
-def evaluate():
-    print("Running evaluate.")
+import hydra
+from omegaconf import DictConfig
+
+
+def evaluate(cfg: DictConfig):
+    """Evaluate function."""
+    print(cfg)
+
+
+@hydra.main(
+    version_base="1.3",
+    config_path="../../config",
+    config_name="evaluate.yaml",
+)
+def main(cfg: DictConfig) -> None:
+    """Main function."""
+    evaluate(cfg)
+
+
+if __name__ == "__main__":
+    # pylint:disable=no-value-for-parameter
+    main()
